1b611e70acbc5b8e1474e75c792189824ca76e4e
Muhamad Rafiki <rafikimuhammad@gmail.com>
2023-10-20 20:02:58 -0500

    working javascripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,20 @@
-calculate.onclick = calculateLoan
+const amount = document.getElementById("amount");
+const rate = document.getElementById("rate");
+const year = document.getElementById("year");
+const result = document.getElementById("result")
+
 function calculateLoan(){
 
     let amountPay = Number(amount.value);
     let rateMounth = Number(rate.value);
     let yearValue = Number(year.value);
+    let result = Number(result.value)
 
     const apr = rateMounth/1200;
     const term = yearValue*12;
     const payment = amt*(apr * Math.pow((1 + apr), term))/(Math.pow((1 + apr), term) - 1);
+   
+    result.value = payment;
 }
+
+calculate.onclick = calculateLoan
